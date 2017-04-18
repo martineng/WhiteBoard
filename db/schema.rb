e85e3d8c6fd2c1c7b2f04ce8bd51e498fc6a004f
martineng <Martin.ECA.6651@gmail.com>
2017-04-18 11:36:28 +0800

    Resolve Migration Pending issues with db:reset and migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170328062531) do
+ActiveRecord::Schema.define(version: 20170418030320) do
 
   create_table "users", force: :cascade do |t|
-    t.string   "username"
-    t.string   "password"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string   "name"
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
