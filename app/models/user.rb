22e3341e39d18c3400f5cd8fe1eae16c7cfb42ff
martineng <Martin.ECA.6651@gmail.com>
2017-04-18 11:10:48 +0800

    Added email verification on User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ApplicationRecord
+  validates :name, presence: true
+
+  # Common REGEX to identify if the email is in valid format.
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 },
+            format { with: VALID_EMAIL_REGEX },
+            uniqueness: { case_sensitive: false }
+
 end
