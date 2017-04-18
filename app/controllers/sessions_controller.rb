ed633870ea9430a7f153a70ea3a933fdf844e808
martineng <Martin.ECA.6651@gmail.com>
2017-04-18 12:10:12 +0800

    Update Session controller and erb files

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,19 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by(email: params[:session][:email].downcase)
+
+    if user && user.authenticate(params[:session][:password])
+      log_in user
+      redirect_to users_url
+    else
+      flash.now[:danger] = 'Invalid credentials.'
+      render 'new'
+    end
   end
 
   def destroy
+    logout
+    redirect_to login_url
   end
 end
