90f253c525476a329a0a405b7824b54b20c49404
martineng <Martin.ECA.6651@gmail.com>
2017-04-18 11:38:51 +0800

    Fix syntax error in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   # Common REGEX to identify if the email is in valid format.
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
-            format { with: VALID_EMAIL_REGEX },
+            format: { with: VALID_EMAIL_REGEX },
             uniqueness: { case_sensitive: false }
 
   has_secure_password
