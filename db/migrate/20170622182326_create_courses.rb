9763b06ae591c611c0c4273210752561c37f0c51
martineng <Martin.ECA.6651@gmail.com>
2017-06-22 11:25:27 -0700

    Create proper course and subject

---
+++ db/migrate/20170622182326_create_courses.rb
@@ -0,0 +1,12 @@
+class CreateCourses < ActiveRecord::Migration[5.0]
+  def change
+    create_table :courses do |t|
+      t.string :course_name
+      t.string :course_code
+      t.string :stream
+      t.string :description
+
+      t.timestamps
+    end
+  end
+end
